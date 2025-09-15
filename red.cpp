e360da67e3ccca9cdb90d8c0cda9747d85ebbc7f
Fakeqq-fake <ila.banderovski4@gmail.com>
2025-09-15 12:10:15 +0300

    Update red.cpp

--- red.cpp
+++ red.cpp
@@ -15,3 +15,40 @@ int main() {
     greetUser(userName);
 
     return 0; 
+
+
+
+
+
+
+
+
+
+
+
+
+#include <iostream>
+
+void findMax(int a, int b) {
+      
+    if (a > b) {
+        std::cout << "The greatest number: " << a << std::endl; 
+    } else {
+        std::cout << "The greatest number: " << b << std::endl;  
+    }
+}
+
+
+int main() {
+    int num1, num2;
+
+    std::cout << "Enter the first integer: ";   
+    std::cin >> num1; 
+
+    std::cout << "Enter the second integer: ";
+    std::cin >> num2; 
+
+    findMax(num1, num2);
+
+    return 0;   
+}
